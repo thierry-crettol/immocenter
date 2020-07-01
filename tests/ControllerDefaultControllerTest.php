e76865dff66449b6384217cb5bee20b359ff76e2
thierry-crettol <cretthie@protonmail.com>
2020-07-01 22:21:13 +0200

    Ajout symfony new project

---
+++ tests/ControllerDefaultControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('html', 'DefaultController');
+    }
+}
